8fb8d2a1cf9ec36cbfa275f1690bf60d18eaae70
Miguel Angel Largo Amado <largoamado@gmail.com>
2022-09-05 19:04:56 -0500

    Correciones de qr.php

--- app/qr.php
+++ app/qr.php
@@ -1,61 +1,59 @@
 <?php
 include("../data-base.php");
 $database = new Database('user-app');
+
 $id_vehicle = $_GET['v'];
-$state = $_GET['s'];
 $username = $_GET['u'];
+$state = $_GET['s'];
+
 $connection = $database->getConnection();
 
-$response = mysqli_query($connection, "select id_vehicle from vehicle
-                                                where username = '$username';");
-$row = $response->fetch_array(MYSQLI_NUM);
+switch ($state) {
+    case 'active':
+        active($connection, $username, $id_vehicle);
+        break;
+    case 'inactive':
+        inactive($connection, $id_vehicle);
+        break;
+}
+
+function active($connection, $username, $id_vehicle)
+{
+    $response = mysqli_query($connection, "select v.state_vehicle, v.username from vehicle v
+                                                where v.id_vehicle = '$id_vehicle';");
+    $row = $response->fetch_array(MYSQLI_NUM);
 
 
-if ($state == 'active') {
-    if ($row != null) {
-        if ($row[0] != $id_vehicle) {
-            echo json_encode(1);
+    if ($row[0] == 'active') {
+        if ($row[1] != $username) {
+            echo 'invalid';
+            $connection->close();
+            return;
+        } else {
+            $response = mysqli_query($connection, "select vu.input_hour from vehicle_has_users vu
+                                                where vu.vehicle_id_vehicle = '$id_vehicle'
+                                                and vu.users_username = '$username';");
+            $row = $response->fetch_array(MYSQLI_NUM);
+            echo $row[0];
+            $connection->close();
             return;
         }
     }
-
-    $response = mysqli_query($connection, "SELECT v.ubicacion_idUbicacion FROM easypark.vehicle v
-                                                    WHERE v.id_vehicle = '$id_vehicle'");
-    $row = $response->fetch_array(MYSQLI_NUM);
-
-    if ($row[0] != "0") {
-        $response = mysqli_query($connection, "SELECT v.username FROM easypark.vehicle v
-                                                    WHERE v.id_vehicle = '$id_vehicle'");
-        $row = $response->fetch_array(MYSQLI_NUM);
-        if ($row[0] == $username) {
-            $response = mysqli_query($connection, "SELECT vu.input_hour FROM easypark.vehicle_has_users vu
-                                                    WHERE vu.users_username = '$username'
-                                                    AND vu.vehicle_id_vehicle = '$id_vehicle'");
-            $row = $response->fetch_array(MYSQLI_NUM);
-            echo json_encode($row[0]);
-        } else {
-            echo json_encode(false);
-        }
-        return;
-    } else {
-        isRead($connection, $id_vehicle, $state);
-    }
-
-
-} else {
-    isRead($connection, $id_vehicle, $state);
+    isRead($connection, $id_vehicle, 'active');
+    echo 'active';
 }
 
+function inactive($connection, $id_vehicle)
+{
+    isRead($connection, $id_vehicle, 'inactive');
+    echo 'inactive';
+}
 
 function isRead($connection, $id_vehicle, $state)
 {
     do {
-        $response = mysqli_query($connection, "select state_vehicle from vehicle where id_vehicle = '$id_vehicle'");
+        $response = mysqli_query($connection, "select v.state_vehicle from vehicle v where v.id_vehicle = '$id_vehicle'");
         $row = $response->fetch_array(MYSQLI_NUM);
     } while ($row[0] != $state);
-    echo json_encode(true);
     $connection->close();
 }
-
-
-
