70563d1e10f12e3211c415d0fe9fdf80af60dd47
Miguel Angel Largo Amado <largoamado@gmail.com>
2022-09-07 16:53:35 -0500

    Se añadio la clase getSensor de Sensor.php

--- sensor/sensor.php
+++ sensor/sensor.php
@@ -1,14 +1,37 @@
 <?php
 include("../data-base.php");
 $database = new Database('user-app');
-
-$name = $_GET['name'];
-$state = $_GET['state'];
-
 $connection = $database->getConnection();
 
-mysqli_query($connection, "UPDATE easypark.ubicacion u
+switch ($_SERVER['REQUEST_URI']) {
+    case '/desktop/parking.php':
+        $name = $_GET['name'];
+        $state = $_GET['state'];
+        updateSensor($connection, $name, $state);
+        break;
+    case '/desktop/parking.php/getSensors':
+        getSensors($connection);
+        break;
+    default:
+        echo 'error route';
+}
+
+
+function updateSensor($connection, $name, $state)
+{
+
+    mysqli_query($connection, "UPDATE easypark.ubicacion u
                                     SET u.estadoUbicacion = '$state'
                                     WHERE u.idUbicacion = '$name';");
+}
+
+function getSensors($connection)
+{
+    $response = mysqli_query($connection, "SELECT u.idUbicacion
+                                    FROM easypark.ubicacion u
+                                    WHERE u.estadoUbicacion = '1';");
+    $row = $response->fetch_array(MYSQLI_NUM);
+    echo json_decode($row);
+}
 
 
