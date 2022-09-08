34f4bf69b57384bbc937e84842be647ab6b73915
Miguel Angel Largo Amado <largoamado@gmail.com>
2022-09-08 08:03:34 -0500

    Modificaciones de return parking

--- app/qr.php
+++ app/qr.php
@@ -27,8 +27,6 @@ function active($connection, $username, $id_vehicle)
     if ($row[0] == 'active') {
         if ($row[1] != $username) {
             echo 'invalid';
-            $connection->close();
-            return;
         } else {
             $response = mysqli_query($connection, "select vu.input_hour from vehicle_has_users vu
                                                 where vu.vehicle_id_vehicle = '$id_vehicle'
@@ -36,10 +34,10 @@ function active($connection, $username, $id_vehicle)
             $row = $response->fetch_array(MYSQLI_NUM);
 
             $p = getParking($connection, $id_vehicle);
-            echo $row[0], $p;
-            $connection->close();
-            return;
+            echo json_encode($row[0], $p);
         }
+        $connection->close();
+        return;
     }
     isRead($connection, $id_vehicle, 'active');
     $p = getParking($connection, $id_vehicle);
