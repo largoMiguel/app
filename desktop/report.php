6d162897ae7374c643a22edc06769af660454352
Miguel Largo <largoamado@gmail.com>
2022-11-03 12:14:22 -0500

    Creacion de la clase reports

--- desktop/report.php
+++ desktop/report.php
@@ -20,11 +20,11 @@ switch ($_SERVER['REQUEST_METHOD']) {
         $id_vehicle = $_POST['username'];
         $username = $_POST['id_vehicle'];
         $date = $_POST['date'];
-        $input_hour = $_POST['input_hour'];
+        $hour = $_POST['hour'];
         $state = $_POST['state'];
         $database = new Database("user-app");
         $connection = $database->getConnection();
-        $response = mysqli_query($connection, "INSERT INTO reports (username, id_vehicle, date, hour, state) VALUES ('$id_vehicle', '$username', '$date', '$input_hour', $state); ");
+        $response = mysqli_query($connection, "INSERT INTO reports (id_vehicle, username, date, hour, state) VALUES ('$id_vehicle', '$username', '$date', '$hour', $state); ");
         $connection->close();
         echo json_encode(true);
         break;
