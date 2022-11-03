0dbd1bd595994f41b6912d76cacb6e62381a0153
Miguel Largo <largoamado@gmail.com>
2022-11-03 05:11:25 -0500

    Creacion del metodo delete vehicle

--- app/vehicle.php
+++ app/vehicle.php
@@ -68,6 +68,6 @@ class VehicleDatabase
         $connection = $database->getConnection();
         mysqli_query($connection, "DELETE FROM hour_input WHERE vehicle_id_vehicle = '$id_vehicle' AND users_username = '$username';");
         $connection->close();
-        return "true";
+        return "trues";
     }
 }
