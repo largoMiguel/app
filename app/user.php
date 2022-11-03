8d1a3e57f9b4a1609aedc74f27bfaa45f996ab28
Miguel Largo <largoamado@gmail.com>
2022-11-03 02:55:41 -0500

    Correcion de roles e insersion user

--- app/user.php
+++ app/user.php
@@ -24,7 +24,7 @@ class UserDatabase
     {
         $database = new Database("user-app");
         $connection = $database->getConnection();
-        $response = mysqli_query($connection, "SELECT * FROM users WHERE username = '$username' and password_user = '$password'");
+        $response = mysqli_query($connection, "SELECT username, name_user, lastname_user,rol FROM users WHERE username = '$username' and password_user = '$password'");
         $row = $response->fetch_array(MYSQLI_NUM);
         $connection->close();
         return $row;
@@ -35,7 +35,7 @@ class UserDatabase
         try {
             $database = new Database("user-app");
             $connection = $database->getConnection();
-            mysqli_query($connection, "INSERT INTO users (username, name_user, lastname_user, password_user) VALUES ($username, '$name', '$lastname','$password')");
+            mysqli_query($connection, "INSERT INTO users (username, name_user, lastname_user, password_user, rol) VALUES ('$username', '$name', '$lastname','$password', 'client')");
             $connection->close();
             return true;
         } catch (Exception $e) {
